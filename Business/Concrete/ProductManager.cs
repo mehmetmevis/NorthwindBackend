5ce056bbbdfc4a84ef324c8e56b80aa9da7528ed
mehmetmevis <mehmet.meviss@gmail.com>
2025-07-25 18:57:07 +0300

    update

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetList()
+        {
+            return _productDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            return _productDal.GetList(p => p.CategoryID == categoryId).ToList();
+        }
+
+        public void Add(Product product)
+        {
+            //Business kodları buraya yazılır.
+            _productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductID == productId);
+        }
+    }
+}
